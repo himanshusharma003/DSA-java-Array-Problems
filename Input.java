9429b00a10c488cd8060d08836deabbbc66c6b8c
HimanshuSharma <himanshusharma003@gmail.com>
2026-02-24 15:06:46 +0530

    added user input greeting program

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a name: ");
+        String name =sc.nextLine();
+        System.out.println("namaste "+name);
+        sc.close();
+
+    }
+}
