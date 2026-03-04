b63e7da7aa2486d49f956c8f78f6b20d1cb4d150
HimanshuSharma <himanshusharma003@gmail.com>
2026-03-04 23:07:52 +0530

    added a return type program

---
+++ Funtion2.java
@@ -0,0 +1,12 @@
+public class Funtion2 {
+    public static void main(String[] args) {
+         String Message=greet();
+         System.out.println(Message);
+         
+        
+    }
+    static String greet(){
+        String Greeting="how are you";
+        return Greeting;
+    }
+}
