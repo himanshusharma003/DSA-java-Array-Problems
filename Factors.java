cd26467cb809e316282e3f8507daf2be4c23cf1f
HimanshuSharma <himanshusharma003@gmail.com>
2026-03-06 14:39:26 +0530

    added factors of a number

---
+++ Factors.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int num=sc.nextInt();
+        System.out.println("the factors of"+num +"are: ");
+        
+        
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                System.out.println(i+" ");
+            }
+        }
+        sc.close();
+    }
+    
+}
