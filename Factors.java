cd1611be4c8a097b5dd6d587e0a8f4ea7d150a31
HimanshuSharma <himanshusharma003@gmail.com>
2026-03-06 14:47:36 +0530

    factors of a num using while loop

--- Factors.java
+++ Factors.java
@@ -6,13 +6,17 @@ public class Factors {
         System.out.println("enter a number: ");
         int num=sc.nextInt();
         System.out.println("the factors of"+num +"are: ");
-        
-        
-        for(int i=1;i<=num;i++){
-            if(num%i==0){
-                System.out.println(i+" ");
+        int divisor=1;
+        while(divisor<=num) {
+            if(num%divisor==0){
+                System.out.println(divisor+" ");
             }
+            divisor++;
+            
         }
+        
+        
+        
         sc.close();
     }
     
